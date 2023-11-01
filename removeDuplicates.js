ba4f15397b1a843c04cee74fbfda411232b52156
Vllaadd <vlladdiimiir@gmail.com>
2023-11-01 01:02:04 -0400

    remove duplicates set up

---
+++ removeDuplicates.js
@@ -0,0 +1,19 @@
+// integer array sorted in non-decreasing order 
+// remove duplicates 
+// order should remain the same 
+// return the number or remainin unique elements, as K 
+
+// create the function
+// nums is the parameter of the function when we call it 
+// iterate through the array 
+//  create pointers since the array is sorted 
+
+function removeDuplicates(nums){
+    for(var i = 0; i = nums.length; i++){
+        if(nums[i]){
+
+        }
+    }
+}
+
+removeDuplicates([2, 4, 5, 7, 4, 6, 5, 9]); 
