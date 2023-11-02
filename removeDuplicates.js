c96b1b7deb91f7a0c04aac7399254117accc891f
Vllaadd <vlladdiimiir@gmail.com>
2023-11-02 04:54:10 -0400

    remove duplicates done

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -6,14 +6,26 @@
 // create the function
 // nums is the parameter of the function when we call it 
 // iterate through the array 
-//  create pointers since the array is sorted 
+//  create pointers since the array is sorted
 
-function removeDuplicates(nums){
-    for(var i = 0; i = nums.length; i++){
-        if(nums[i]){
 
+
+function removeDuplicates(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let k = 1; // Initialize the count of unique elements to 1
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[k - 1]) {
+            nums[k] = nums[i]; // Overwrite the next unique element
+            k++;
         }
     }
+
+    return k;
 }
+
+
 
 removeDuplicates([2, 4, 5, 7, 4, 6, 5, 9]); 
