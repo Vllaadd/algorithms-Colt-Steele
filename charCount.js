7cfb621a713494d06c03572cac41f66f9601ed95
Vllaadd <vlladdiimiir@gmail.com>
2024-02-10 05:39:36 -0500

    charCount two solutions

--- charCount.js
+++ charCount.js
@@ -21,9 +21,7 @@ function charCount(str) {
     for (var char of str) {
         char = char.toLowerCase()
         if (/[a-z0-9]/.test(char)) {
-            obj[char]++;
-        }else {
-            obj[char] = 1;
+       obj[char] = ++obj[char] || 1;
         };
     }
     return obj;
