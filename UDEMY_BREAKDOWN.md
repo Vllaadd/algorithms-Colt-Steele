c5ed3cf6d3c9ddcbd7c894db7d55d0c6907978c1
Vllaadd <vlladdiimiir@gmail.com>
2024-02-10 04:44:51 -0500

    code_breakdown updated

--- UDEMY_BREAKDOWN.md
+++ UDEMY_BREAKDOWN.md
@@ -5,3 +5,13 @@
 4. **Solve or Simplify**
 5. **Look Back and Refactor**
 
+
+# charCount 
+1. **for-loop** 
+- The for loop is a fundamental construct in programming used to iterate over elements in a collection, such as na array or a string. It allows executing a block of code repeatedly until a specified condition is met. 
+- There are different ways to implement a for loop in JavaScript, as demonstrated in the provided code snippet: 
+- traditional for loop: 
+`for(var i = 0; i < str.length; i++)` 
+- for ... of loop 
+`for(var char of str)` 
+
--- charCount.js
+++ charCount.js
@@ -4,19 +4,28 @@ function charCount(str) {
     //loop over string, for each character
     for (var i = 0; i < str.length; i++) {
         var char = str[i].toLowerCase()
-        //if the char is a number/letter AND is a key in object, add one to count
+
         if (result[char] > 0) {
             result[char]++;
         }
-        //if the char is a number/letter AND not in object, add it to object and set value
         else {
             result[char] = 1;
         };
     }
-    //return object
     return result;
 }
 
 //different approach 
-
+function charCount(str) {
+    var obj = {};
+    for (var char of str) {
+        char = char.toLowerCase()
+        if (/[a-z0-9]/.test(char)) {
+            obj[char]++;
+        }else {
+            obj[char] = 1;
+        };
+    }
+    return obj;
+}
 
