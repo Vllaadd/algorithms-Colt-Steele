4eb1a2f5307bf477ada8f1e5ec08578316ff2775
Vllaadd <vlladdiimiir@gmail.com>
2024-02-17 01:47:55 -0500

    name changed

---
+++ binarysearch.js
@@ -0,0 +1,34 @@
+//given a sorted array of integers, whire a function called search
+//that accepts a value and returns the index where the value passed to the function is located. if the value is not found, return -1
+
+
+function search(array, val) {
+    let min = 0;
+    let max = array.length - 1;
+
+    while (min <= max) {
+        let middle = Math.floor(min + max) / 2;
+        let currentElement = array[middle];
+
+        if (array[middle] < val) {
+            min = middle + 1;
+        } else if (array[middle] > val) {
+            max = middle - 1;
+        } else {
+            return middle;
+        }
+    }
+
+    return -1;
+}
+
+//naive solution
+//function search(arr, val){
+// for(let i = 0; i < arr.length; i++){
+// if(arr[i]) === val
+// {
+// return if;
+// }
+// }
+// return -1;
+// }
