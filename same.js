be8d271bb5de9eb9cf12b60779e545d913283c84
Vllaadd <vlladdiimiir@gmail.com>
2024-02-20 05:36:08 -0500

    same updated

--- same.js
+++ same.js
@@ -1,3 +1,7 @@
+// write a function called same, which accepts two arrays. The function 
+//should return true if every value in the array has it's corresponding value 
+//squared in the second array. The frequency of values must be the same 
+
 // naive solution
 function same(arr1, arr2){
     if(arr1.length !== arr2.length){
